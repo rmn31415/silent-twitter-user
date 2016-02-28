05f020af4f14859c6f62b0bf70f6037a6d13889d
Matsuyuki(rmn_e) <matsuyuki.eca@gmail.com>
2016-02-28 15:50:50 +0900

    静的HTML返却とポートカスタマイズ

--- main.js
+++ main.js
@@ -2,23 +2,30 @@ var express = require( "express" );
 var twitter = require( "twitter" );
 var app = express();
 
-var token = [
+var tokenList = [
   { name: "Consumer Key",        value: process.env.TWITTER_CONSUMER_KEY },
   { name: "Consumer Secret",     value: process.env.TWITTER_CONSUMER_SECRET },
   { name: "Access Token Key",    value: process.env.TWITTER_ACCESS_TOKEN_KEY },
   { name: "Access Token Secret", value: process.env.TWITTER_ACCESS_TOKEN_SECRET },
 ];
 
-app.get( '/', function( req, res ) {
-  res.send( "Hello, World!" );
-} );
+var port = 3000;
+if( process.env.NODE_PORT !== void 0 ) {
+  port = parseInt( process.env.NODE_PORT, 10 );
+  if( isNaN( port ) ) {
+    console.log( "[ERROR] Unrecognized NODE_PORT: " + process.env.NODE_PORT );
+    process.exit();
+  }
+}
 
-token.forEach( function( item ) {
+app.use( '/', express.static( 'public' ) );
+
+tokenList.forEach( function( item ) {
   if( item.value === void 0 /* undefined */ ) {
     console.log( "[ERROR] " + item.name + " is undefined. exit." );
     process.exit();
   }
 } );
 
-app.listen(3000);
-console.log( "server is running on http://localhost:3000/" );
+app.listen( port );
+console.log( "server is running on http://localhost:" + port + "/" );
