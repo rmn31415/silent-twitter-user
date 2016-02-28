e63eb46038a7fed154389d33bd3bc3064240301b
Matsuyuki(rmn_e) <matsuyuki.eca@gmail.com>
2016-02-28 15:36:24 +0900

    add: example of token configuration file

--- README.md
+++ README.md
@@ -3,10 +3,11 @@
 
 ## How to use
 
-``` sh
-$ node install # only first time to launch
-$ node main.js
-```
+1. `run_sample.sh` を `run.sh` にコピーしてください。
+2. `run.sh` をエディタで開いて各種Twitterのトークンを設定してください。
+3. `run.sh` は `.gitignore` に含まれており、 gitのリポジトリ内で無視対象になっています。
+4. `node install` して依存しているライブラリを入れてください。
+5. `./run.sh` でサーバが起動します。
 
 ## LICENSE
 MIT License
--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 var express = require( "express" );
+var twitter = require( "twitter" );
 var app = express();
+
+var token = [
+  { name: "Consumer Key",        value: process.env.TWITTER_CONSUMER_KEY },
+  { name: "Consumer Secret",     value: process.env.TWITTER_CONSUMER_SECRET },
+  { name: "Access Token Key",    value: process.env.TWITTER_ACCESS_TOKEN_KEY },
+  { name: "Access Token Secret", value: process.env.TWITTER_ACCESS_TOKEN_SECRET },
+];
 
 app.get( '/', function( req, res ) {
   res.send( "Hello, World!" );
+} );
+
+token.forEach( function( item ) {
+  if( item.value === void 0 /* undefined */ ) {
+    console.log( "[ERROR] " + item.name + " is undefined. exit." );
+    process.exit();
+  }
 } );
 
 app.listen(3000);
