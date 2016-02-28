748483c36e97fe3f7e14b7638a4047b00655a0f4
Matsuyuki(rmn_e) <matsuyuki.eca@gmail.com>
2016-02-28 16:11:44 +0900

    とりあえず検索してみる

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var express = require( "express" );
-var twitter = require( "twitter" );
+var Twitter = require( "twitter" );
 var app = express();
 
 var tokenList = [
@@ -9,7 +9,22 @@ var tokenList = [
   { name: "Access Token Secret", value: process.env.TWITTER_ACCESS_TOKEN_SECRET },
 ];
 
+tokenList.forEach( function( item ) {
+  if( item.value === void 0 /* undefined */ ) {
+    console.log( "[ERROR] " + item.name + " is undefined. exit." );
+    process.exit();
+  }
+} );
+
+var client = new Twitter( {
+  consumer_key:       process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret:    process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key:    process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+} );
+
 var port = 3000;
+
 if( process.env.NODE_PORT !== void 0 ) {
   port = parseInt( process.env.NODE_PORT, 10 );
   if( isNaN( port ) ) {
@@ -20,12 +35,16 @@ if( process.env.NODE_PORT !== void 0 ) {
 
 app.use( '/', express.static( 'public' ) );
 
-tokenList.forEach( function( item ) {
-  if( item.value === void 0 /* undefined */ ) {
-    console.log( "[ERROR] " + item.name + " is undefined. exit." );
-    process.exit();
-  }
+app.get( '/test', function( req, res ) {
+  client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response){
+    console.log( "Tweets:" + JSON.stringify( tweets ));
+    console.log( "Error:" + JSON.stringify(error ));
+  });
+  res.send( "Hello, World!" );
 } );
+
 
 app.listen( port );
 console.log( "server is running on http://localhost:" + port + "/" );
+
+
